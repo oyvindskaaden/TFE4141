0a9765d973c8b86cf8d951b26d9500c4d0051cab
Øyvind <oyvindskaaden@gmail.com>
2022-10-18 11:43:26 +0200

    Added files for the core module

---
+++ project/rsa_vhdl_files/RSA_accelerator/source/rsa_core_control.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/18/2022 11:22:47 AM
+-- Design Name: 
+-- Module Name: rsa_core_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- This is the control unit for the rsa core module
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity rsa_core_control is
+--  Port ( );
+end rsa_core_control;
+
+architecture Behavioral of rsa_core_control is
+
+begin
+
+
+end Behavioral;
---
+++ project/rsa_vhdl_files/RSA_accelerator/source/rsa_core_datapath.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/18/2022 11:22:47 AM
+-- Design Name: 
+-- Module Name: rsa_core_datapath - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- This is the datapath for the rsa core
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity rsa_core_datapath is
+--  Port ( );
+end rsa_core_datapath;
+
+architecture Behavioral of rsa_core_datapath is
+
+begin
+
+
+end Behavioral;
