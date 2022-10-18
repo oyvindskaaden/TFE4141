29a344834cdd1271b201476717e769b8206d26ce
Øyvind <oyvindskaaden@gmail.com>
2022-10-18 11:42:53 +0200

    Added files for the multi mod module

---
+++ project/rsa_vhdl_files/Exponentiation/source/multi_mod_control.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/18/2022 11:26:16 AM
+-- Design Name: 
+-- Module Name: multi_mod_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity multi_mod_control is
+--  Port ( );
+end multi_mod_control;
+
+architecture Behavioral of multi_mod_control is
+
+begin
+
+
+end Behavioral;
---
+++ project/rsa_vhdl_files/Exponentiation/source/multi_mod_datapath.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/18/2022 11:31:01 AM
+-- Design Name: 
+-- Module Name: multi_mod_datapath - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity multi_mod_datapath is
+--  Port ( );
+end multi_mod_datapath;
+
+architecture Behavioral of multi_mod_datapath is
+
+begin
+
+
+end Behavioral;
