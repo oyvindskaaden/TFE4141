99c8a19d6891d2760ba75b42f6a0f5b51d31d9ae
torjeikenes <torje.n.eikenes@gmail.com>
2022-10-28 14:04:24 +0200

    Started on multi mod control logic

--- project/rsa_vhdl_files/Exponentiation/source/multi_mod_control.vhd
+++ project/rsa_vhdl_files/Exponentiation/source/multi_mod_control.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_signed.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,8 +33,14 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity multi_mod_control is
+    generic (
+		C_block_size : integer := 256;
+		COUNTER_WIDTH : integer := 8
+	);
     Port (
-
+    
+        mm_data_in_ready    : in std_logic;     
+        mm_data_out_ready   : out std_logic;     
 
         -- Datapath control logic
         A_reg_load  : out std_logic;
@@ -56,8 +63,75 @@ entity multi_mod_control is
 end multi_mod_control;
 
 architecture Behavioral of multi_mod_control is
-
+    signal cnt_en       : std_logic;
+    signal cnt_out      : std_logic_vector(COUNTER_WIDTH-1 downto 0);
+    signal cnt_reset_n  : std_logic;
+    
+    
+    type state is (IDLE, SETUP, RUNNING, DONE);
+    signal curr_state, next_state : state;
 begin
 
+    counter : entity work.counter 
+    generic map (
+      COUNTER_WIDTH => COUNTER_WIDTH)
+    port map (
+      clk           => clk,
+      reset_n       => cnt_reset_n,
+      cnt_en        => cnt_en,
+      y             => cnt_out);
+
+
+    A_reg_load <= mm_data_in_ready;
+    N_reg_load <= mm_data_in_ready;
+    
+
+    process(mm_data_in_ready, cnt_out) begin
+        if(mm_data_in_ready = '1') then
+            cnt_reset_n <= '0';
+            
+            if(cnt_out = 255) then
+                cnt_en <= '0';
+            else
+                cnt_en <= '1';
+            end if;
+        else
+            cnt_en <= '0';
+            cnt_reset_n <= '1';
+            
+        end if; 
+
+    end process;
+
+
+    fsmComb : process(curr_state) begin
+        case (curr_state) is
+        when IDLE =>
+            cnt_en <= '0';
+            cnt_reset_n <= '0';
+            
+            
+            if(mm_data_in_ready = '1') then
+                next_state <= RUNNING;
+            else
+                next_state <= IDLE;
+            end if;
+        when RUNNING =>
+            cnt_en <= '1';
+            cnt_reset_n <= '1';
+            
+            if(cnt_out = 255) then
+                next_state <= DONE;
+            else
+                next_state <= RUNNING;
+            end if;
+            
+        when DONE =>
+            cnt_en <= '0';
+            cnt_reset_n <= '0';
+            
+            next_state <= IDLE;
+        
+    end process;
 
 end Behavioral;
